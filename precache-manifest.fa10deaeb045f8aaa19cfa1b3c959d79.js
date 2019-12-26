e0594bc19bc88aef86243fb6f9d442c38cd9b7d6
Pivotass <pivoorigami@gmail.com>
2019-12-26 16:58:40 +0100

    Updates

---
+++ precache-manifest.fa10deaeb045f8aaa19cfa1b3c959d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0755179f81d23f58a329d94f9ce073fe",
+    "url": "/wewine-front/index.html"
+  },
+  {
+    "revision": "fde824e94f630c4d10b3",
+    "url": "/wewine-front/static/css/main.fb7ad05d.chunk.css"
+  },
+  {
+    "revision": "1be66461e81b74ce49a0",
+    "url": "/wewine-front/static/js/2.d46de5a7.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/wewine-front/static/js/2.d46de5a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fde824e94f630c4d10b3",
+    "url": "/wewine-front/static/js/main.d44b2a29.chunk.js"
+  },
+  {
+    "revision": "78f233940c8604d3755c",
+    "url": "/wewine-front/static/js/runtime-main.87661dcf.js"
+  }
+]);
